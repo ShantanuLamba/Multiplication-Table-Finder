54d63365c0c07f58be81f633f7adb09d636d0353
ShantanuLamba <sl.group958@gmail.com>
2021-02-04 19:18:46 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function result(){
+    var str = document.getElementById("numbr").value;
+    var val = str*2;
+    var vall = str*3;
+    var valf = str*4;
+    var valfi = str*5;
+    var valsix = str*6;
+    var vals = str*7;
+    var vale= str*8;
+    var valn = str*9;
+    var valt = str*10;
+
+    document.getElementById("output").innerHTML = str+"x1="+str;
+    document.getElementById("output2").innerHTML = str+"x2= "+val;
+    document.getElementById("output3").innerHTML = str+"x3= "+vall;
+    document.getElementById("output4").innerHTML = str+"x4= "+valf;
+    document.getElementById("output5").innerHTML = str+"x5= "+valfi;
+    document.getElementById("output6").innerHTML = str+"x6= "+valsix;
+    document.getElementById("output7").innerHTML = str+"x7= "+vals;
+    document.getElementById("output8").innerHTML = str+"x8= "+vale;
+    document.getElementById("output9").innerHTML = str+"x9= "+valn;
+    document.getElementById("output10").innerHTML = str+"x10= "+valt;
+}
+
+
